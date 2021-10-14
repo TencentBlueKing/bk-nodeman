7488ed89b5dffefdb5aab280abcb6838e44dad07
crayon <873217631@qq.com>
2021-10-14 21:11:26 +0800

    feature: 选择接入点原子改造及测试，屏蔽旧原子单测 (close #188)

--- apps/mock_data/common_unit/host.py
+++ apps/mock_data/common_unit/host.py
@@ -8,7 +8,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from apps.node_man import constants
+from apps.node_man import constants, models
 
 from .. import utils
 
@@ -16,9 +16,60 @@ DEFAULT_HOST_ID = 1
 
 DEFAULT_IP = "127.0.0.1"
 
+AP_MODEL_DATA = {
+    "id": 1,
+    "name": "默认接入点",
+    "ap_type": "system",
+    "region_id": "test",
+    "city_id": "test",
+    "btfileserver": [{"inner_ip": DEFAULT_IP, "outer_ip": DEFAULT_IP}],
+    "dataserver": [{"inner_ip": DEFAULT_IP, "outer_ip": DEFAULT_IP}],
+    "taskserver": [{"inner_ip": DEFAULT_IP, "outer_ip": DEFAULT_IP}],
+    "zk_hosts": [{"zk_ip": DEFAULT_IP, "zk_port": "2181"}],
+    "zk_account": "zk_account",
+    "zk_password": "zk_password",
+    "package_inner_url": f"http://{DEFAULT_IP}:80/download",
+    "package_outer_url": f"http://{DEFAULT_IP}:80/download",
+    "nginx_path": "",
+    "agent_config": {
+        "linux": {
+            "dataipc": "/var/run/ipc.state.report",
+            "log_path": "/var/log/gse",
+            "run_path": "/var/run/gse",
+            "data_path": "/var/lib/gse",
+            "temp_path": "/tmp",
+            "setup_path": "/usr/local/gse",
+            "hostid_path": "/var/lib/gse/host/hostid",
+        },
+        "windows": {
+            "dataipc": 47000,
+            "log_path": "c:\\gse\\logs",
+            "run_path": "c:\\gse\\logs",
+            "data_path": "c:\\gse\\data",
+            "temp_path": "C:\\tmp",
+            "setup_path": "c:\\gse",
+            "hostid_path": "C:\\gse\\data\\host\\hostid",
+        },
+    },
+    "status": None,
+    "description": "GSE默认接入点",
+    "is_enabled": True,
+    "is_default": True,
+    "creator": ["admin"],
+    "port_config": constants.GSE_PORT_DEFAULT_VALUE,
+    "proxy_package": [
+        "gse_client-windows-x86.tgz",
+        "gse_client-windows-x86_64.tgz",
+        "gse_client-linux-x86.tgz",
+        "gse_client-linux-x86_64.tgz",
+        "gse_client-aix-powerpc.tgz",
+    ],
+    "outer_callback_url": f"http://{DEFAULT_IP}:10300/backend",
+}
+
 HOST_MODEL_DATA = {
     "bk_host_id": DEFAULT_HOST_ID,
-    "bk_biz_id": utils.DEFAULT_BK_BIZ_NAME,
+    "bk_biz_id": utils.DEFAULT_BK_BIZ_ID,
     "bk_cloud_id": constants.DEFAULT_CLOUD,
     "inner_ip": DEFAULT_IP,
     "outer_ip": DEFAULT_IP,
@@ -44,4 +95,15 @@ IDENTITY_MODEL_DATA = {
     "extra_data": {},
     # 密码保留天数
     "retention": 1,
+}
+
+
+PROCESS_STATUS_MODEL_DATA = {
+    "bk_host_id": DEFAULT_HOST_ID,
+    "name": models.ProcessStatus.GSE_AGENT_PROCESS_NAME,
+    "status": constants.ProcStateType.RUNNING,
+    "version": "1.0.0",
+    "proc_type": constants.ProcType.AGENT,
+    "is_latest": True,
+    "source_type": models.ProcessStatus.SourceType.DEFAULT,
 }
