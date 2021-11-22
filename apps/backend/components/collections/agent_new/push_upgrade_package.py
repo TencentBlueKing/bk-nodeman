db0220c6bc4f0d158b45c249a13dca43baf70d78
crayon <873217631@qq.com>
2021-11-22 10:20:09 +0800

    feature: 下发升级包 (closed #285)

---
+++ apps/backend/components/collections/agent_new/push_upgrade_package.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-节点管理(BlueKing-BK-NODEMAN) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+import os.path
+from typing import List
+
+from django.conf import settings
+
+from apps.node_man import constants, models
+
+from .base import AgentCommonData, AgentTransferPackageService
+
+
+class PushUpgradePackageService(AgentTransferPackageService):
+    def get_file_target_path(self, data, common_data: AgentCommonData, host: models.Host) -> str:
+        return common_data.host_id__ap_map[host.bk_host_id].get_agent_config(host.os_type)["temp_path"]
+
+    def get_file_list(self, data, common_data: AgentCommonData, host: models.Host) -> List[str]:
+
+        package_type = ("client", "proxy")[host.node_type == constants.NodeType.PROXY]
+        download_path = common_data.host_id__ap_map[host.bk_host_id].nginx_path or settings.DOWNLOAD_PATH
+        agent_upgrade_package_name = f"gse_{package_type}-{host.os_type.lower()}-{host.cpu_arch}_upgrade.tgz"
+
+        file_names: List[str] = [agent_upgrade_package_name]
+
+        # Windows机器需要添加解压文件
+        if host.os_type == constants.OsType.WINDOWS:
+            file_names.extend(["7z.dll", "7z.exe"])
+
+        return [os.path.join(download_path, file_name) for file_name in file_names]
