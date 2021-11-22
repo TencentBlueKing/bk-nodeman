343a7ac8014a854dfc5d4a7d29a08eadc7af811a
crayon <873217631@qq.com>
2021-11-22 10:19:03 +0800

    feature: 检测 GSE Server 到 Proxy 策略 (closed #289)

--- apps/mock_data/api_mkd/job/utils.py
+++ apps/mock_data/api_mkd/job/utils.py
@@ -8,6 +8,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from common.api import JobApi
 
 from ... import utils
 from . import unit
@@ -24,6 +25,8 @@ class JobApiMockClient(utils.BaseMockClient):
         create_credential_return=None,
         create_file_source_return=None,
     ):
+
+        super().__init__()
 
         # 操作类接口返回结构基本一致，如果没有具体指定，使用默认值返回
         default_op_interface_return = utils.MockReturn(
@@ -42,3 +45,8 @@ class JobApiMockClient(utils.BaseMockClient):
         self.get_job_instance_ip_log = self.generate_magic_mock(mock_return_obj=get_job_instance_ip_log_return)
         self.create_credential = self.generate_magic_mock(mock_return_obj=create_credential_return)
         self.create_file_source = self.generate_magic_mock(mock_return_obj=create_file_source_return)
+
+        # 记录接口调用
+        self.fast_execute_script = self.call_recorder.start(self.fast_execute_script, key=JobApi.fast_execute_script)
+        self.fast_transfer_file = self.call_recorder.start(self.fast_transfer_file, key=JobApi.fast_transfer_file)
+        self.push_config_file = self.call_recorder.start(self.push_config_file, key=JobApi.push_config_file)
