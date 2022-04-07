501df7dfda78ee6bb733b860b5865f6cea810c91
crayon <873217631@qq.com>
2022-04-07 16:12:28 +0800

    bugfix: 修复 Proxy 重载配置导致临时文件路径置空的问题 (closed #627)

--- apps/backend/components/collections/agent_new/update_install_info.py
+++ apps/backend/components/collections/agent_new/update_install_info.py
@@ -8,9 +8,11 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from collections import ChainMap
 from typing import List
 
 from apps.node_man import models
+from apps.utils.basic import filter_values
 
 from .base import AgentBaseService, AgentCommonData
 
@@ -23,13 +25,16 @@ class UpdateInstallInfoService(AgentBaseService):
             bk_host_id = host_info["bk_host_id"]
             host_obj = common_data.host_id_obj_map[bk_host_id]
 
-            extra_data = {
-                "peer_exchange_switch_for_agent": host_info.get("peer_exchange_switch_for_agent", True),
-                "bt_speed_limit": host_info.get("bt_speed_limit", 0),
+            default_extra_data = {"peer_exchange_switch_for_agent": True, "bt_speed_limit": 0}
+            added_extra_data = {
+                "peer_exchange_switch_for_agent": host_info.get("peer_exchange_switch_for_agent"),
+                "bt_speed_limit": host_info.get("bt_speed_limit"),
+                "data_path": host_info.get("data_path"),
             }
-            if host_info.get("data_path"):
-                extra_data.update({"data_path": host_info["data_path"]})
-            host_obj.extra_data = extra_data
+            # 移除 None 值
+            added_extra_data = filter_values(added_extra_data)
+            # 同名配置覆盖优先级：added_extra_data（新增配置）> host_obj.extra_data（已有配置）> default_extra_data（默认配置）
+            host_obj.extra_data = dict(ChainMap(added_extra_data, host_obj.extra_data, default_extra_data))
             host_obj.ap_id = host_info["ap_id"]
             hosts_to_be_updated.append(host_obj)
         models.Host.objects.bulk_update(hosts_to_be_updated, fields=["extra_data", "ap_id"], batch_size=self.batch_size)
