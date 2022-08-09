4ce4db2fdb153fab101229161b9b3ee86854ac95
xcwang <1366993017@qq.com>
2022-08-09 19:07:14 +0800

    optimization: Agent 安装脚本移除启动插件逻辑 (closed #727)

--- script_tools/setup_proxy.sh
+++ script_tools/setup_proxy.sh
@@ -536,22 +536,6 @@ setup_py36 () {
     )
 }
 
-start_basic_gse_plugin () {
-    log start_plugin START "start gse plugin: basereport, processbeat"
-    cd "$AGENT_SETUP_PATH/../plugins/bin" || fail start_plugin FAILED "change directory to $AGENT_SETUP_PATH/../plugins/bin failed"
-
-    if [[ -x ./basereport ]]; then
-        ./stop.sh basereport
-        ./start.sh basereport || fail start_plugin FAILED "basereport start failed."
-    fi
-    if [[ -x ./processbeat ]]; then
-        ./stop.sh processbeat
-        ./start.sh processbeat || fail start_plugin FAILED "processbeat start failed."
-    fi
-
-    log start_plugin DONE "gse plugin 'basereport,processbeat start done."
-}
-
 download_pkg () {
     local f http_status path
 
@@ -854,7 +838,6 @@ for step in check_env \
             remove_proxy \
             remove_agent_if_exists \
             setup_proxy \
-            start_basic_gse_plugin \
             setup_startup_scripts \
             check_deploy_result; do
     $step
