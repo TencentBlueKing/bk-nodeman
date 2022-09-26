a3072ff5bff7f7878dc318e6a4ba44065520b5f2
GONGONGONG <506419689@qq.com>
2022-09-26 16:03:01 +0800

    optimization: 复制IP默认以换行符进行分隔 (closed #1140)

--- frontend/src/setup/directives.ts
+++ frontend/src/setup/directives.ts
@@ -3,8 +3,11 @@ import cursor from '@/common/cursor';
 import freezingPage from '@/common/freezing-page';
 import authority from '@/common/authority';
 import testAnchor from '@/common/test-anchor';
+import VueClipboard from 'vue-clipboard2';
 
 Vue.use(cursor);
 Vue.use(freezingPage);
 Vue.use(authority);
 Vue.use(testAnchor);
+VueClipboard.config.autoSetContainer = true;
+Vue.use(VueClipboard);
