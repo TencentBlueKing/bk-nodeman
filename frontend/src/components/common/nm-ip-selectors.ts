52d32f60ba55dfb48f8574fa0095cb126e3b4382
GONGONGONG <506419689@qq.com>
2022-09-28 16:06:55 +0800

    feature: IP选择权主策略

--- frontend/src/components/common/nm-ip-selectors.ts
+++ frontend/src/components/common/nm-ip-selectors.ts
@@ -2,6 +2,7 @@ import create from '@blueking/ip-selector/dist/vue2.6.x';
 import '@blueking/ip-selector/dist//styles/vue2.6.x.css';
 import * as IpChooserTopo from '@/api/modules/ipchooser_topo';
 import * as IpChooserHost from '@/api/modules/ipchooser_host';
+import { INodeType } from '@/types/plugin/plugin-type';
 
 export interface IMeta {
   bk_biz_id: number
@@ -13,6 +14,20 @@ export interface INode {
   instance_id: number
   object_id: 'module' | 'set' | 'biz'
   meta: IMeta
+}
+export interface IHost {
+  host_id: number
+  meta: IMeta
+}
+export interface ITarget {
+  // node_type = 'INSTANCE' => bk_host_id  ||  'TOPO' => bk_obj_id && bk_inst_id
+  bk_biz_id: number,
+  bk_obj_id?: 'set' | 'module'
+  bk_inst_id?: number
+  bk_host_id?: number
+  biz_inst_id?: string
+  path?: string
+  children?: ITarget[]
 }
 
 export interface ITreeItem extends INode {
@@ -38,41 +53,79 @@ export interface IQuery {
   search_content?: string
   node_list: INode[]
 }
+export interface ISelectorValue {
+  dynamic_group_list: Dictionary[]
+  host_list: IHost[]
+  node_list: INode[]
+}
+
+/**
+ * 转换成标准的IP选择器的选中数据
+ */
+export function toSelectorNode(nodes: ITarget[], nodeType: INodeType) {
+  if (!nodeType || nodes.some(node => node.meta)) return nodes;
+  if (nodeType === 'INSTANCE') {
+    return nodes.map(item => ({
+      host_id: item.bk_host_id,
+      meta: {
+        scope_type: 'biz',
+        scope_id: `${item.bk_biz_id}`,
+        bk_biz_id: item.bk_biz_id,
+      },
+    }));
+  }
+  return nodes.map(item => ({
+    object_id: item.bk_obj_id,
+    instance_id: item.bk_inst_id,
+    meta: {
+      scope_type: 'biz',
+      scope_id: `${item.bk_biz_id}`,
+      bk_biz_id: item.bk_biz_id,
+    },
+  }));
+}
+
+/**
+ * 转换为策略需要的选中数据
+ */
+export function toStrategyNode(nodes: Array<INode | IHost>, nodeType: INodeType) {
+  if (!nodeType || nodes.some(node => !node.meta)) return nodes;
+  if (nodeType === 'INSTANCE') {
+    return nodes.map((item: IHost) => ({
+      bk_biz_id: item.meta.bk_biz_id,
+      bk_host_id: item.host_id,
+    }));
+  }
+  return nodes.map((item: INode) => ({
+    bk_obj_id: item.object_id,
+    bk_inst_id: item.instance_id,
+    bk_biz_id: item.meta.bk_biz_id,
+  }));
+}
 
 
 const CustomSettingsService = {
   fetchAll: () => Promise.resolve([]),
   update: () => Promise.resolve([]),
+  fetchConfig: () => Promise.resolve([]),
 };
 
 export default {
   name: 'NmIpSelector',
   props: {
+    panelList: {
+      type: Array,
+      default: () => [],
+    },
+    value: {
+      type: Object as ISelectorValue,
+      default: () => ({}),
+    },
     action: {
       type: String,
       default: 'strategy_create',
       // type: Array,
       // default: () => [],
-    },
-    nodeType: {
-      type: String,
-      default: 'TOPO',
-    },
-    checkedData: {
-      type: Array,
-      default: () => [],
-    },
-    previewWidth: {
-      type: String,
-      default: '',
-    },
-    height: {
-      type: String,
-      default: '',
-    },
-    customizeLimit: {
-      type: Boolean,
-      default: true,
     },
   },
   data() {
@@ -83,7 +136,7 @@ export default {
   },
   created() {
     this.instance = create({
-      panelList: ['dynamicTopo', 'staticTopo', 'manualInput'],
+      panelList: this.panelList,
       unqiuePanelValue: true,
       nameStyle: 'kebabCase', // 'camelCase' | 'kebabCase'
       fetchTopologyHostCount: this.fetchTopologyHostCount, // 拉取topology
@@ -91,14 +144,14 @@ export default {
       fetchNodesQueryPath: this.fetchNodesQueryPath, // 动态拓扑 - 勾选节点
       fetchHostAgentStatisticsNodes: this.fetchHostAgentStatisticsNodes, // 动态拓扑 - 勾选节点
       fetchTopologyHostIdsNodes: IpChooserTopo.queryHostIdInfos, // 根据多个拓扑节点与搜索条件批量分页查询所包含的主机 ID 信息(跨页全选)
-      fetchHostsDetails: this.fetchHostsDetails,
+      fetchHostsDetails: this.fetchHostsDetails, // 静态 - IP选择回显(host_id查不到时显示失效)
       fetchHostCheck: this.fetchHostCheck, // 手动输入 - 根据用户手动输入的`IP`/`IPv6`/`主机名`/`host_id`等关键字信息获取真实存在的机器信息
       // fetchDynamicGroups: IpChooserTopo.fetchDynamicGroup,
       // fetchHostsDynamicGroup: IpChooserTopo.fetchDynamicGroupHost,
       // fetchHostAgentStatisticsDynamicGroups: IpChooserTopo.fetchBatchGroupAgentStatistics,
       fetchCustomSettings: CustomSettingsService.fetchAll,
       updateCustomSettings: CustomSettingsService.update,
-      fetchConfig: () => Promise.resolve({}),
+      fetchConfig: () => CustomSettingsService.fetchConfig,
     });
   },
   mounted() {
@@ -107,7 +160,6 @@ export default {
   methods: {
     // 拉取topology
     async fetchTopologyHostCount(node?: INode): Promise<ITreeItem[]> {
-      console.log(node, '---------------tree');
       const params = {
         action: 'strategy_create',
       };
@@ -116,10 +168,9 @@ export default {
       } else {
         params.all_scope =  true;
       }
-      let list = await IpChooserTopo.trees(params);
-      list = list.map(item => (({ ...item, lazy: !node })));
+      const list = await IpChooserTopo.trees(params);
       if (node) {
-        list = list.find(item => item.instance_id === node.instance_id)?.child || [];
+        return Promise.resolve(list[0].child);
       }
       // this.topology = list;
 
@@ -137,13 +188,23 @@ export default {
 
     // 动态拓扑 - 勾选节点(查询多个节点拓扑路径)
     fetchNodesQueryPath(node: IFetchNode): Promise<Array<INode>[]> {
-      console.log('fetchNodesQueryPath', node);
-      return IpChooserTopo.queryPath({ action: this.action, node_list: node.node_list });
+      // console.log('fetchNodesQueryPath', node);
+      return IpChooserTopo.queryPath({ action: this.action, node_list: node.node_list }, {
+        cancelPrevious: false,
+      });
     },
     // 动态拓扑 - 勾选节点(获取多个拓扑节点的主机 Agent 状态统计信息)
-    fetchHostAgentStatisticsNodes(node: IFetchNode): Promise<{ agent_statistics: IStatistics, node: INode }[]> {
-      console.log('fetchHostAgentStatisticsNodes', node);
-      return IpChooserTopo.agentStatistics({ action: this.action, node_list: node.node_list });
+    async fetchHostAgentStatisticsNodes(node: IFetchNode): Promise<{ agent_statistics: IStatistics, node: INode }[]> {
+      // console.log('fetchHostAgentStatisticsNodes', node);
+      const res = await IpChooserTopo.agentStatistics({ action: this.action, node_list: node.node_list });
+      res.forEach((item) => {
+        Object.assign(item.agent_statistics, {
+          ...item.agent_statistics,
+          not_alive_count: item.agent_statistics.no_alive_count,
+        });
+        delete item.agent_statistics.no_alive_count;
+      });
+      return Promise.resolve(res);
     },
     fetchHostsDetails(node) {
       console.log('fetchHostsDetails', node);
@@ -171,7 +232,7 @@ export default {
       ref: 'ipSelector',
       props: {
         mode: 'section',
-        value: this.checkedData,
+        value: this.value,
         // service: {
         //   fetchNodesQueryPath() {} // 灰度
         // }
