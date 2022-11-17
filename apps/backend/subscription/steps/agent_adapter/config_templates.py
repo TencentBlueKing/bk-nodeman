06a6ea141caf6b0e8652d584956228bd9efb7068
crayon <873217631@qq.com>
2022-11-17 19:26:25 +0800

    feature: Agent 2.0 GSE 文件服务对接 (closed #1239)

--- apps/backend/subscription/steps/agent_adapter/config_templates.py
+++ apps/backend/subscription/steps/agent_adapter/config_templates.py
@@ -11,7 +11,7 @@ specific language governing permissions and limitations under the License.
 
 GSE_AGENT_CONFIG_TMPL: str = """
 {
-    "run_mode": __BK_GSE_RUN_MODE__,
+    "run_mode": "__BK_GSE_RUN_MODE__",
     "cloud_id": __BK_GSE_CLOUD_ID__,
     "zone_id": "__BK_GSE_ZONE_ID__",
     "city_id": "__BK_GSE_CITY_ID__",
@@ -52,6 +52,10 @@ GSE_AGENT_CONFIG_TMPL: str = """
     "data": {
         "ipc": "__BK_GSE_DATA_IPC__",
         "ipc_thread_num": __BK_GSE_DATA_IPC_RECV_THREAD_NUM__
+    },
+    "file": {
+        "max_transfer_speed_mb_per_sec": __BK_GSE_FILE_MAX_TRANSFER_SPEED_MB_PER_SEC__,
+        "max_transfer_concurrent_num": __BK_GSE_FILE_MAX_TRANSFER_CONCURRENT_NUM__
     },
     "logger": {
         "path": "__BK_GSE_LOG_PATH__",
@@ -121,6 +125,70 @@ GSE_DATA_PROXY_CONFIG_TMPL: str = """
                 ]
             }
         ]
+    }
+}
+"""
+
+
+GSE_FILE_PROXY_CONFIG_TEMPL: str = """
+{
+    "run_mode": "proxy",
+    "cloud_id": __BK_GSE_CLOUD_ID__,
+    "agent": {
+        "bind_ip": "__BK_GSE_FILE_AGENT_BIND_IP__",
+        "bind_port": __BK_GSE_FILE_AGENT_BIND_PORT__,
+        "bind_port_v1": __BK_GSE_FILE_AGENT_BIND_PORT_V1__,
+        "advertise_ipv4": "__BK_GSE_FILE_AGENT_ADVERTISE_IPV4__",
+        "advertise_ipv6": "__BK_GSE_FILE_AGENT_ADVERTISE_IPV6__",
+        "thread_num": __BK_GSE_FILE_AGENT_THREAD_NUM__,
+        "tls_ca_file": "__BK_GSE_FILE_AGENT_TLS_CA_FILE__",
+        "tls_passwd_file": "__BK_GSE_FILE_AGENT_TLS_PASSWORD_FILE__",
+        "tls_cert_file": "__BK_GSE_FILE_AGENT_TLS_CERT_FILE__",
+        "tls_key_file": "__BK_GSE_FILE_AGENT_TLS_KEY_FILE__"
+    },
+    "bittorrent": {
+        "bind_ip": "__BK_GSE_FILE_BITTORRENT_BIND_IP__",
+        "bind_port": __BK_GSE_FILE_BITTORRENT_BIND_PORT__,
+        "tracker_bind_port": __BK_GSE_FILE_BITTORRENT_TRACKER_BIND_PORT__,
+        "speed_limit_mb_per_sec": __BK_GSE_FILE_BITTORRENT_SPEED_LIMIT_MB_PER_SEC__
+    },
+    "topology": {
+        "bind_ip": "__BK_GSE_FILE_TOPOLOGY_BIND_IP__",
+        "bind_port": __BK_GSE_FILE_TOPOLOGY_BIND_PORT__,
+        "thrift_bind_port": __BK_GSE_FILE_TOPOLOGY_THRIFT_BIND_PORT__,
+        "advertise_ip": "__BK_GSE_FILE_TOPOLOGY_ADVERTISE_IP__",
+        "thread_num": __BK_GSE_FILE_TOPOLOGY_THREAD_NUM__,
+        "tls_ca_file": "__BK_GSE_FILE_TOPOLOGY_TLS_CA_FILE__",
+        "tls_passwd_file": "__BK_GSE_FILE_TOPOLOGY_TLS_PASSWORD_FILE__",
+        "tls_svr_cert_file": "__BK_GSE_FILE_TOPOLOGY_TLS_SVR_CERT_FILE__",
+        "tls_svr_key_file": "__BK_GSE_FILE_TOPOLOGY_TLS_SVR_KEY_FILE__",
+        "tls_cli_cert_file": "__BK_GSE_FILE_TOPOLOGY_TLS_CLI_CERT_FILE__",
+        "tls_cli_key_file": "__BK_GSE_FILE_TOPOLOGY_TLS_CLI_KEY_FILE__",
+        "links": [
+            {
+                "target_ip": "__BK_GSE_FILE_PROXY_UPSTREAM_IP__",
+                "target_port": __BK_GSE_FILE_PROXY_UPSTREAM_PORT__,
+                "report_ip": "__BK_GSE_FILE_PROXY_REPORT_IP__",
+                "report_port": __BK_GSE_FILE_PROXY_REPORT_PORT__
+            }
+        ]
+    },
+    "cache": {
+        "dirs": "__BK_GSE_FILE_CACHE_DIRS__",
+        "expired_time_sec": __BK_GSE_FILE_CACHE_EXPIRED_TIME_SEC__
+    },
+    "metric": {
+        "exporter_bind_ip": "__BK_GSE_FILE_METRIC_EXPORTER_BIND_IP__",
+        "exporter_bind_port": __BK_GSE_FILE_METRIC_EXPORTER_BIND_PORT__,
+        "exporter_thread_num": __BK_GSE_FILE_METRIC_EXPORTER_THREAD_NUM__
+    },
+    "logger": {
+        "path": "__BK_GSE_LOG_PATH__",
+        "level": "__BK_GSE_LOG_LEVEL__",
+        "filesize_mb": __BK_GSE_LOG_FILESIZE_MB__,
+        "filenum": __BK_GSE_LOG_FILENUM__,
+        "rotate": __BK_GSE_LOG_ROTATE__,
+        "flush_interval_ms": __BK_GSE_LOG_FLUSH_INTERVAL_MS__
     }
 }
 """
