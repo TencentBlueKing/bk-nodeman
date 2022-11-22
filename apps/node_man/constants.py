946d8c27d99c17b1d1adea6730d6e5044d53c48e
crayon <873217631@qq.com>
2022-11-22 16:40:42 +0800

    feature: 适配 CMDB 主机寻址方式枚举值变更 (closed #1260)

--- apps/node_man/constants.py
+++ apps/node_man/constants.py
@@ -875,8 +875,8 @@ class CmdbObjectId:
 class CmdbAddressingType(EnhanceEnum):
     """寻址方式"""
 
-    STATIC = "0"
-    DYNAMIC = "1"
+    STATIC = "1"
+    DYNAMIC = "2"
 
     @classmethod
     def _get_member__alias_map(cls) -> Dict[Enum, str]:
