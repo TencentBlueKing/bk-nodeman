c02d01783a1dd2fd6b8d3992dfbd8ff5a717d4cb
zhangzhw8 <826035498@qq.com>
2022-12-09 17:35:45 +0800

    feature: Agent 升级功能问题修复（Windows 压缩工具下发，Linux 脚本 CPU 架构校验等）(close #1328)

--- apps/backend/components/collections/agent_new/push_upgrade_package.py
+++ apps/backend/components/collections/agent_new/push_upgrade_package.py
@@ -10,7 +10,7 @@ specific language governing permissions and limitations under the License.
 """
 
 import os.path
-from typing import List
+from typing import List, Tuple
 
 from django.conf import settings
 
@@ -23,25 +23,26 @@ class PushUpgradeFileService(AgentTransferFileService):
     def get_file_target_path(self, data, common_data: AgentCommonData, host: models.Host) -> str:
         return common_data.host_id__ap_map[host.bk_host_id].get_agent_config(host.os_type)["temp_path"]
 
-    def get_upgrade_package_source_path(self, common_data: AgentCommonData, host: models.Host):
+    def get_upgrade_package_source_path(self, common_data: AgentCommonData, host: models.Host) -> Tuple[str, str]:
         """
         获取升级包源路径
         """
         # 1.x 升级到 1.x，使用老到路径，升级包直接放在 download 目录下
-        download_path = common_data.host_id__ap_map[host.bk_host_id].nginx_path or settings.DOWNLOAD_PATH
+        agent_path = root_path = common_data.host_id__ap_map[host.bk_host_id].nginx_path or settings.DOWNLOAD_PATH
         if not common_data.agent_step_adapter.is_legacy:
             # 2.x 升级到 2.x，根据操作系统、CPU 架构等组合路径
-            download_path = os.path.join(download_path, "agent", host.os_type.lower(), host.cpu_arch.lower())
-        return download_path
+            agent_path = os.path.join(root_path, "agent", host.os_type.lower(), host.cpu_arch.lower())
+        return root_path, agent_path
 
     def get_file_list(self, data, common_data: AgentCommonData, host: models.Host) -> List[str]:
-        download_path = self.get_upgrade_package_source_path(common_data, host)
+        root_path, agent_path = self.get_upgrade_package_source_path(common_data, host)
         agent_upgrade_package_name = self.get_agent_upgrade_pkg_name(common_data, host=host)
 
-        file_names: List[str] = [agent_upgrade_package_name]
+        file_list: List[str] = [os.path.join(agent_path, agent_upgrade_package_name)]
 
         # Windows 机器需要添加解压工具
         if host.os_type == constants.OsType.WINDOWS:
-            file_names.extend(["7z.dll", "7z.exe"])
+            for zip_tool in ["7z.dll", "7z.exe"]:
+                file_list.append(os.path.join(root_path, zip_tool))
 
-        return [os.path.join(download_path, file_name) for file_name in file_names]
+        return file_list
