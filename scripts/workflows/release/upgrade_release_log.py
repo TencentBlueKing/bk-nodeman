e9fdcd68a30ac2c76b3c08e01afd964628540845
crayon <873217631@qq.com>
2022-12-22 20:17:02 +0800

    optimization: 提供更规范的 CI 流程 (closed #1354)

--- scripts/workflows/release/upgrade_release_log.py
+++ scripts/workflows/release/upgrade_release_log.py
@@ -3,41 +3,36 @@ import datetime
 import getopt
 import os
 import sys
-from collections import defaultdict
 from pathlib import Path
-from typing import Dict, List, Union
-
-import yaml
-from get_prerelease_version import get_prerelease_version
+from typing import Dict, Union
 
 HELP_TEXT = """
 生成发布日志
 通用参数：
         [ -h, --help                  [可选] "说明文档" ]
-        [ -d, --dev-log-root          [必选] "开发日志目录路径" ]
         [ -r, --release-log-root      [必选] "发布日志路径" ]
-        [ -p, --release-md-path       [可选] "发布日志readme文件路径，默认取 {release-log-root}/release.md" ]
-        [ -v, --prerelease-version    [可选] "预发布版本，默认取dev-log-root最新的记录" ]
+        [ -p, --release-md-path       [可选] "发布日志readme文件路径，默认取 {release-log-root}/readme.md" ]
+        [ -v, --prerelease-version    [必选] "预发布版本" ]
+        [ -l, --changelog-path        [必选] "变更日志路径" ]
 """
 
 
 def extract_params(argv) -> Dict[str, Union[str, bool, int, float]]:
     try:
         opts, args = getopt.getopt(
-            argv, "hd:r:v:p:", ["dev-log-root=", "release-log-root=", "prerelease-version", "release-md-path", "help"]
+            argv,
+            "hr:v:p:l:",
+            ["release-log-root=", "prerelease-version=", "release-md-path=", "changelog-path=", "help"],
         )
     except getopt.GetoptError:
         print(HELP_TEXT)
         sys.exit(2)
 
-    sh_params = {"dev-log-root": None, "release-log-root": None, "prerelease-version": None, "release-md-path": None}
+    sh_params = {"release-log-root": None, "prerelease-version": None, "release-md-path": None, "changelog-path": None}
     for opt, arg in opts:
         if opt in ("h", "--help"):
             print(HELP_TEXT)
             sys.exit(2)
-
-        elif opt in ("-d", "--dev-log-root"):
-            sh_params["dev-log-root"] = arg
 
         elif opt in ("-r", "--release-log-root"):
             sh_params["release-log-root"] = arg
@@ -47,40 +42,25 @@ def extract_params(argv) -> Dict[str, Union[str, bool, int, float]]:
 
         elif opt in ("-p", "--release-md-path"):
             sh_params["release-md-path"] = arg
+
+        elif opt in ("-l", "--changelog-path"):
+            sh_params["changelog-path"] = arg
     return sh_params
 
 
 if __name__ == "__main__":
     params = extract_params(sys.argv[1:])
-    dev_log_root = params["dev-log-root"]
+    changelog_path = params["changelog-path"]
     release_log_root = params["release-log-root"]
-    release_md_path = params["release-md-path"] or os.path.join(release_log_root, "release.md")
+    prerelease_version = params["prerelease-version"]
+    release_md_path = params["release-md-path"] or os.path.join(release_log_root, "readme.md")
 
-    # 获取等待发布的版本号
-    prerelease_version = params["prerelease-version"] or get_prerelease_version(dev_log_root=dev_log_root)
-    # 具体到某个版本的开发日志
-    dev_yaml_dir_path = os.path.join(dev_log_root, prerelease_version)
-    # 列举归档的yaml文件
-    dev_yaml_file_name_list = os.listdir(dev_yaml_dir_path)
-    # 根据pr类型对开发日志文本进行聚合
-    msgs_group_by_pr_type: Dict[str, List[str]] = defaultdict(list)
-
-    for dev_yaml_file_name in dev_yaml_file_name_list:
-        dev_yaml_file_path = os.path.join(dev_yaml_dir_path, dev_yaml_file_name)
-
-        try:
-            with open(file=dev_yaml_file_path, encoding="utf-8") as dev_yaml_fs:
-                dev_yaml = yaml.safe_load(dev_yaml_fs)
-            for pr_type, user_msgs in dev_yaml.items():
-                msgs_group_by_pr_type[pr_type].extend(user_msgs)
-        except Exception:
-            # 忽略解析错误的开发日志
-            continue
+    with open(file=changelog_path, mode="r", encoding="utf-8") as changelog_fs:
+        # 整体数据量不大，全部读取
+        changelog = changelog_fs.read()
 
     # 拼接日志
-    release_text = f"\n## {prerelease_version} - {datetime.date.today()} \n"
-    for pr_type, msgs in msgs_group_by_pr_type.items():
-        release_text += f"\n\n### {pr_type}: \n  * " + "\n  * ".join(msgs)
+    release_text = f"\n## {prerelease_version} - {datetime.date.today()} \n\n{changelog}"
 
     # 如果发布日志目录路径不存在，逐层进行创建，并且忽略已创建的层级（exist_ok）
     if not os.path.exists(release_log_root):
