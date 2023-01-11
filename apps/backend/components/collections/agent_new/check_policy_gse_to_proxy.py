1aacc5f41a49d597ad22bd7c0f9e5b284181c018
crayon <873217631@qq.com>
2023-01-11 20:37:58 +0800

    sprintfix: 修复探测文件服务到 2.0 Proxy 10020/10030 端口不通的问题 (fixed #1239)

--- apps/backend/components/collections/agent_new/check_policy_gse_to_proxy.py
+++ apps/backend/components/collections/agent_new/check_policy_gse_to_proxy.py
@@ -10,6 +10,7 @@ specific language governing permissions and limitations under the License.
 """
 from typing import Any, Dict, List, Union
 
+from django.conf import settings
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
@@ -48,11 +49,15 @@ class CheckPolicyGseToProxyService(AgentExecuteScriptService):
 
     def get_script_content(self, data, common_data: AgentCommonData, host: models.Host) -> str:
         port_config = common_data.host_id__ap_map[host.bk_host_id].port_config
+        btsvr_thrift_port = port_config.get("btsvr_thrift_port")
         return REACHABLE_SCRIPT_TEMPLATE % {
             "proxy_ip": host.outer_ip or host.outer_ipv6,
-            "btsvr_thrift_port": port_config.get("btsvr_thrift_port"),
-            "bt_port": port_config.get("bt_port"),
-            "tracker_port": port_config.get("tracker_port"),
+            "btsvr_thrift_port": btsvr_thrift_port,
+            # Agent 2.0 bt_port & tracker_port 不属于 Proxy <> Server 的策略
+            # 在上述情况下冗余探测 btsvr_thrift_port
+            # Refer: https://github.com/TencentBlueKing/bk-nodeman/issues/1239
+            "bt_port": (port_config.get("bt_port"), btsvr_thrift_port)[settings.BKAPP_ENABLE_DHCP],
+            "tracker_port": (port_config.get("tracker_port"), btsvr_thrift_port)[settings.BKAPP_ENABLE_DHCP],
             "msg": _("请检查出口IP是否正确或策略是否开通"),
         }
 
@@ -63,11 +68,20 @@ class CheckPolicyGseToProxyService(AgentExecuteScriptService):
             host = common_data.host_id_obj_map[bk_host_id]
             ap = common_data.host_id__ap_map[bk_host_id]
             port_config = ap.port_config
+
+            if settings.BKAPP_ENABLE_DHCP:
+                port_polices: List[str] = [f"{port_config.get('btsvr_thrift_port')}(tcp)"]
+            else:
+                port_polices: List[str] = [
+                    f"{port_config.get('btsvr_thrift_port')}(tcp)",
+                    f"{port_config.get('bt_port')}-{port_config.get('tracker_port')}(tcp/udp)",
+                ]
+
             self.log_info(
                 sub_inst_id,
                 _(
-                    "请确保 GSE BT File Server 公网IP（{file_endpoints}）到 Proxy 出口IP（{proxy_outer_ip}）的 "
-                    "{btsvr_thrift_port}(tcp), {bt_port}-{tracker_port}(tcp/udp) 访问策略正常。\n"
+                    "请确保 GSE File Server 公网IP（{file_endpoints}）到 Proxy 出口IP（{proxy_outer_ip}）的 "
+                    "{port_polices} 访问策略正常。\n"
                     "另外请注意：\n"
                     "1. 可执行 curl ipinfo.io 确认「出口IP」\n"
                     "2. 不支持多台 Proxy 使用同一个「出口IP」\n"
@@ -75,9 +89,7 @@ class CheckPolicyGseToProxyService(AgentExecuteScriptService):
                 ).format(
                     file_endpoints=",".join(ap.file_endpoint_info.outer_hosts),
                     proxy_outer_ip=host.outer_ip or host.outer_ipv6,
-                    btsvr_thrift_port=port_config.get("btsvr_thrift_port"),
-                    bt_port=port_config.get("bt_port"),
-                    tracker_port=port_config.get("tracker_port"),
+                    port_polices=", ".join(port_polices),
                 ),
             )
 
