3620b5bcb831e18cc2a3e04f2e6d7a7cb0c550ae
yunchao <wyyalt@live.com>
2023-04-20 12:41:43 +0800

    feature:  插件配置公共常量支持 (closed #1500)

--- apps/backend/subscription/tools.py
+++ apps/backend/subscription/tools.py
@@ -970,6 +970,22 @@ def get_plugin_path(plugin_name: str, target_host: models.Host, agent_config: Di
     return plugin_path
 
 
+def get_plugin_common_constants(plugin_name: str) -> Dict:
+    """
+    获取插件配置公共常量
+    https://github.com/TencentBlueKing/bk-nodeman/issues/1500
+    :param plugin_name: 插件名称
+    """
+    plugin_common_constants = models.GlobalSettings.get_config(
+        key=models.GlobalSettings.KeyEnum.PLUGIN_COMMON_CONSTANTS.value,
+        default={},
+    )
+    # 获取特定插件公共常量
+    plugin_constants = plugin_common_constants.get(plugin_name, {})
+    plugin_constants.update({"global": plugin_common_constants.get("global", {})})
+    return plugin_constants
+
+
 def get_all_subscription_steps_context(
     subscription_step: models.SubscriptionStep,
     instance_info: Dict,
@@ -1011,10 +1027,11 @@ def get_all_subscription_steps_context(
                 "inner_ip": target_host.inner_ip,
                 "outer_ip": target_host.outer_ip,
                 "login_ip": target_host.login_ip,
-            }
+            },
+            # 获取插件配置公共常量
+            "constants": get_plugin_common_constants(plugin_name),
         },
     )
-
     # 深拷贝一份，避免原数据后续被污染
     context = copy.deepcopy(context)
     return context
