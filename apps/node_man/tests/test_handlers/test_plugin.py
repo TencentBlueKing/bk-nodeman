88c10890302e76c48449061e78a3c1ef69ec085f
郭秋平 <446105468@qq.com>
2023-10-23 17:15:17 +0800

    feature:  plugin_search 接口支持返回部分接入点信息 (closed #1858)

--- apps/node_man/tests/test_handlers/test_plugin.py
+++ apps/node_man/tests/test_handlers/test_plugin.py
@@ -17,6 +17,7 @@ from apps.node_man import constants as const
 from apps.node_man.handlers.plugin import PluginHandler
 from apps.node_man.models import (
     GsePluginDesc,
+    Host,
     Packages,
     PluginConfigTemplate,
     ProcControl,
@@ -348,6 +349,28 @@ class TestPlugin(TestCase):
 
         create_cloud_area(number, creator="admin")
         host_to_create, _, _ = create_host(number)
+
+        # 测试正常查询
+        hosts = PluginHandler.list(
+            {
+                "pagesize": page_size,
+                "page": 1,
+            },
+        )
+
+        for host in hosts["list"]:
+            self.assertIn(host["setup_path"], ["/usr/local/gse", "c:\\gse"])
+
+    @patch("apps.node_man.handlers.cmdb.CmdbHandler.cmdb_or_cache_biz", cmdb_or_cache_biz)
+    @patch("apps.node_man.handlers.cmdb.client_v2", MockClient)
+    def test_setup_path_using_invalid_ap(self):
+        number = 1000
+        page_size = 10
+
+        create_cloud_area(number, creator="admin")
+        host_to_create, _, _ = create_host(number)
+
+        Host.objects.all().update(ap_id=-1)
 
         # 测试正常查询
         hosts = PluginHandler.list(
