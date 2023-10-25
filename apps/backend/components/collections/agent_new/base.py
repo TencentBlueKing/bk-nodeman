61f7b6c6f07c9ee7222f695511d090aa42222560
wyyalt <wyyalt@live.com>
2023-10-25 15:01:32 +0800

    optimization:  安装Gse2.0 查询Agent状态优化  (closed #1883)

--- apps/backend/components/collections/agent_new/base.py
+++ apps/backend/components/collections/agent_new/base.py
@@ -99,6 +99,7 @@ class AgentBaseService(BaseService, metaclass=abc.ABCMeta):
             subscription_instance_ids=common_data.subscription_instance_ids,
             sub_inst_id__host_id_map=common_data.sub_inst_id__host_id_map,
             host_id__sub_inst_id_map=common_data.host_id__sub_inst_id_map,
+            sub_inst_id__sub_inst_obj_map=common_data.sub_inst_id__sub_inst_obj_map,
             # Agent 新增的公共数据
             default_ap=default_ap,
             host_id__ap_map=host_id__ap_map,
