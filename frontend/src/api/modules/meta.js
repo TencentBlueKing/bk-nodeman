0d30763eb77441c47c932128955a6df184f655e1
GONGONGONG <506419689@qq.com>
2023-11-17 11:58:17 +0800

    feat(front): 任务历史筛选条件拉取优化 (closed #1856)

--- frontend/src/api/modules/meta.js
+++ frontend/src/api/modules/meta.js
@@ -1,6 +1,6 @@
 import { request } from '../base';
 
-export const getFilterCondition = request('GET', 'api/meta/filter_condition/');
+export const getFilterCondition = request('POST', 'api/meta/filter_condition/');
 export const jobSettings = request('POST', 'api/meta/job_settings/');
 export const retrieveGlobalSettings = request('GET', 'api/meta/global_settings/');
 
