73f242a2b5fcaaa644eeaa0f53fa9f78517baab0
郭秋平 <446105468@qq.com>
2023-11-29 12:05:13 +0800

    feature:  提供 Agent 包管理后台基础接口 (closed #1683)

--- apps/node_man/serializers/package_manage.py
+++ apps/node_man/serializers/package_manage.py
@@ -11,21 +11,14 @@ specific language governing permissions and limitations under the License.
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
-from apps.core.tag.models import Tag
 from apps.exceptions import ValidationError
-from apps.node_man.constants import BUILT_IN_TAG_NAMES, GsePackageCode
-from apps.node_man.models import GsePackageDesc
+from apps.node_man.constants import GsePackageCode
 
 
 class TagsSerializer(serializers.Serializer):
+    id = serializers.CharField()
     name = serializers.CharField()
-    description = serializers.CharField()
-
-
-class ParentTagSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    description = serializers.CharField()
-    children = TagsSerializer(many=True)
+    children = serializers.ListField()
 
 
 class ConditionsSerializer(serializers.Serializer):
@@ -33,103 +26,24 @@ class ConditionsSerializer(serializers.Serializer):
     values = serializers.ListField()
 
 
-class BasePackageSerializer(serializers.Serializer):
-    def get_tags(self, obj, to_top=False):
-        agent_project_ids = GsePackageDesc.objects.filter(project=obj.project).values_list("id", flat=True)
-        tags = Tag.objects.filter(target_id__in=agent_project_ids, target_version=obj.version).values_list(
-            "name", "description"
-        )
-        if to_top:
-            tags = [{"name": name, "description": description} for name, description in tags]
-            return TagsSerializer(list(tags), many=True).data
-
-        built_in_tags, custom_tags = self.split_builtin_tags_and_custom_tags(tags)
-        parent_tags = [
-            {"name": "builtin", "description": "内置标签", "children": built_in_tags},
-            {"name": "custom", "description": "自定义标签", "children": custom_tags},
-        ]
-        self.filter_no_children_parent_tag(parent_tags)
-        return ParentTagSerializer(parent_tags, many=True).data
-
-    @classmethod
-    def split_builtin_tags_and_custom_tags(cls, tags):
-        """将标签拆分为内置的和自定义的"""
-        built_in_tags, custom_tags = [], []
-        for name, description in tags:
-            if name in BUILT_IN_TAG_NAMES:
-                built_in_tags.append({"name": name, "description": description})
-            else:
-                custom_tags.append({"name": name, "description": description})
-
-        return built_in_tags, custom_tags
-
-    @classmethod
-    def filter_no_children_parent_tag(cls, parent_tags):
-        for i in range(len(parent_tags) - 1, -1, -1):
-            if not parent_tags[i].get("children"):
-                parent_tags.pop(i)
-
-    @classmethod
-    def get_description(cls, obj):
-        return GsePackageDesc.objects.filter(project=obj.project).first().description
-
-
-class PackageSerializer(BasePackageSerializer):
+class PackageSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     pkg_name = serializers.CharField()
     version = serializers.CharField()
     os = serializers.CharField()
     cpu_arch = serializers.CharField()
-    tags = serializers.SerializerMethodField()
-    created_by = serializers.CharField()
-    created_time = serializers.DateTimeField()
+    tags = TagsSerializer(many=True)
+    creator = serializers.CharField()
+    pkg_ctime = serializers.DateTimeField()
     is_ready = serializers.BooleanField()
 
 
-class FilterConditionPackageSerializer(BasePackageSerializer):
-    version = serializers.CharField()
-    tags = serializers.SerializerMethodField()
-    created_by = serializers.CharField()
-    is_ready = serializers.BooleanField()
-
-
-class QuickFilterConditionPackageSerializer(BasePackageSerializer):
-    version = serializers.CharField()
-    os = serializers.CharField()
-    cpu_arch = serializers.CharField()
-
-
-class VersionDescPackageSerializer(BasePackageSerializer):
+class PackageDescSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     version = serializers.CharField()
-    tags = serializers.SerializerMethodField()
+    tags = TagsSerializer(many=True)
+    packages = PackageSerializer(many=True)
     is_ready = serializers.BooleanField()
-    description = serializers.SerializerMethodField()
-    pkg_name = serializers.CharField()
-
-    def get_tags(self, obj, to_top=False):
-        return super().get_tags(obj, to_top=True)
-
-
-class DescPackageSerializer(BasePackageSerializer):
-    version = serializers.CharField()
-    tags = serializers.SerializerMethodField()
-    is_ready = serializers.BooleanField()
-    description = serializers.SerializerMethodField()
-
-
-class PackageDescSerializer(BasePackageSerializer):
-    id = serializers.IntegerField()
-    version = serializers.CharField()
-    tags = serializers.SerializerMethodField()
-    # packages = NameDescPackageSerializer(many=True)
-    packages = serializers.SerializerMethodField()
-    is_ready = serializers.BooleanField()
-    # description = serializers.SerializerMethodField()
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        return data
 
 
 class ListResponseSerializer(serializers.Serializer):
@@ -137,20 +51,13 @@ class ListResponseSerializer(serializers.Serializer):
     list = PackageSerializer(many=True)
 
 
-class PackageDescResponseSerializer(serializers.Serializer):
+class PackageDescResponseSerialiaer(serializers.Serializer):
     total = serializers.IntegerField()
     list = PackageDescSerializer(many=True)
 
 
 class OperateSerializer(serializers.Serializer):
     is_ready = serializers.BooleanField()
-
-    def update(self, instance, validated_data):
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.save()
-        return instance
 
 
 class QuickSearchSerializer(serializers.Serializer):
@@ -183,34 +90,25 @@ class ParseSerializer(serializers.Serializer):
 
 class ParseResponseSerializer(serializers.Serializer):
     class ParsePackageSerializer(serializers.Serializer):
-        project = serializers.ChoiceField(choices=["agent", "proxy"], required=False)
-        pkg_name = serializers.CharField(required=False)
-        pkg_abs_path = serializers.CharField(source="pkg_absolute_path")
-        version = serializers.CharField(required=False)
+        module = serializers.ChoiceField(choices=["agent", "proxy"])
+        pkg_name = serializers.CharField()
+        pkg_abs_path = serializers.CharField()
+        version = serializers.CharField()
         os = serializers.CharField()
         cpu_arch = serializers.CharField()
-        config_templates = serializers.ListField(default=[])
-
-        def to_representation(self, instance):
-            data = super().to_representation(instance)
-            data["project"] = self.context.get("project", "")
-            data["pkg_name"] = self.context.get("pkg_name", "")
-            data["version"] = self.context.get("version", "")
-            return data
+        config_templates = serializers.ListField()
 
     description = serializers.CharField()
     packages = ParsePackageSerializer(many=True)
 
 
 class AgentRegisterSerializer(serializers.Serializer):
-    # class RegisterPackageSerializer(serializers.Serializer):
-    #     pkg_abs_path = serializers.CharField()
-    #     tags = serializers.ListField()
-    #
-    # is_release = serializers.BooleanField()
-    # packages = RegisterPackageSerializer(many=True)
-    file_name = serializers.CharField()
-    tags = serializers.ListField(child=serializers.CharField())
+    class RegisterPackageSerializer(serializers.Serializer):
+        pkg_abs_path = serializers.CharField()
+        tags = serializers.ListField()
+
+    is_release = serializers.BooleanField()
+    packages = RegisterPackageSerializer(many=True)
 
 
 class AgentRegisterTaskSerializer(serializers.Serializer):
