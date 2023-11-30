ea70c4595eccd62f829de8b5b81ba84422804f7f
v_dcdding(丁超达) <1151627903@qq.com>
2023-11-30 10:45:00 +0800

    bugfix:  同步插件状态版本未匹配到 (closed #1996)

--- apps/adapters/tests/api/gse/test_v2.py
+++ apps/adapters/tests/api/gse/test_v2.py
@@ -29,3 +29,43 @@ class TestGseV2ApiHelper(CustomBaseTestCase):
         gse_process_version_sample = "\n 7  \n. \n5 .\n1 \n \n-r\nc 1 5 \n 8 \n "
         version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
         self.assertEqual("7.5.1-rc158", version_example)
+
+        gse_process_version_sample = "mesg:ttynamefailed:Inappropriateioctlfordevic"
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = "bind:Addressalreadyinuse unix_listener:cannot"
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = "2022/06/1416:57:19maxprocs:LeavingGOMAXPROCS="
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = "stty:standardinput:Input/outputerror1.17.68"
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = "rootè´¦å·ä¸,èªå¨åæ¢å°dailybuildè´¦å·"
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = "Hey!Theprogram'go'iscurrentlynotinstalled.You"
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = " Hey!Thepr o \n gram'go'iscurrentlynotinstalled.You \n \n  "
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = None
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = ""
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
+
+        gse_process_version_sample = " "
+        version_example = GseApiBaseHelper.get_version(version_str=gse_process_version_sample)
+        self.assertEqual("special", version_example)
