fc61060356ebe21286ce920ed56f9992a019533b
yunchao <wyyalt@live.com>
2024-05-07 15:19:06 +0800

    评审会: 支持「未分配管控区域」- agent安装整体改造 (closed #2025)

--- apps/mock_data/api_mkd/cmdb/utils.py
+++ apps/mock_data/api_mkd/cmdb/utils.py
@@ -51,6 +51,7 @@ class CCApiMockClient(utils.BaseMockClient):
         push_host_identifier_return=None,
         find_host_identifier_push_result_return=None,
         list_biz_hosts_return=None,
+        batch_update_host_all_properties_return=None,
     ):
         super(CCApiMockClient, self).__init__()
         self.search_business = self.generate_magic_mock(mock_return_obj=search_business_return)
@@ -65,6 +66,9 @@ class CCApiMockClient(utils.BaseMockClient):
             mock_return_obj=find_host_identifier_push_result_return
         )
         self.list_biz_hosts = self.generate_magic_mock(mock_return_obj=list_biz_hosts_return)
+        self.batch_update_host_all_properties = self.generate_magic_mock(
+            mock_return_obj=batch_update_host_all_properties_return
+        )
 
         # 记录接口调用
         self.bind_host_agent = self.call_recorder.start(self.bind_host_agent, key=CCApi.bind_host_agent)
