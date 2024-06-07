9712a06ea183b231f563da67bfe1818e31a81239
HubuHito <hubu_zht@163.com>
2024-06-07 18:54:50 +0800

    feat: 支持子系统嵌入(closed #2274)

--- frontend/src/router/navigation-config.ts
+++ frontend/src/router/navigation-config.ts
@@ -12,7 +12,7 @@ export const navConfig: INavConfig[] = [
           {
             title: 'nav_Agent状态',
             icon: 'nc-state',
-            path: '/agent-manager/status',
+            path: 'agent-manager/status',
             name: 'agentStatus',
           },
         ],
@@ -23,25 +23,25 @@ export const navConfig: INavConfig[] = [
           {
             title: 'nav_插件状态',
             icon: 'nc-plug-in',
-            path: '/plugin-new/list', // '/plugin-manager/list',
+            path: 'plugin-new/list', // '/plugin-manager/list',
             name: 'plugin',
           },
           {
             title: 'nav_插件部署',
             icon: 'nc-strategy',
-            path: '/plugin-manager/rule',
+            path: 'plugin-manager/rule',
             name: 'pluginRule',
           },
           {
             title: 'nav_插件包',
             icon: 'nc-package-2',
-            path: '/plugin-manager/package',
+            path: 'plugin-manager/package',
             name: 'pluginPackage',
           },
           {
             title: 'nav_资源配额',
             icon: 'nc-icon-control-fill',
-            path: '/plugin-manager/resource-quota',
+            path: 'plugin-manager/resource-quota',
             name: 'resourceQuota',
           },
         ],
@@ -52,7 +52,7 @@ export const navConfig: INavConfig[] = [
           {
             title: 'nav_任务历史',
             icon: 'nc-history',
-            path: '/task-list',
+            path: 'task-list',
             name: 'taskList',
           },
         ],
@@ -61,7 +61,7 @@ export const navConfig: INavConfig[] = [
   },
   {
     title: 'nav_管控区域管理',
-    path: '/cloud-manager',
+    path: 'cloud-manager',
     name: 'cloudManager',
   },
   {
@@ -74,13 +74,13 @@ export const navConfig: INavConfig[] = [
       {
         title: 'nav_GSE环境管理',
         icon: 'nc-environment',
-        path: '/global-config/gse-config',
+        path: 'global-config/gse-config',
         name: 'gseConfig',
       },
       {
         title: 'nav_自监控',
         icon: 'nc-monitor',
-        path: '/global-config/healthz',
+        path: 'global-config/healthz',
         name: 'healthz',
       },
     ],
