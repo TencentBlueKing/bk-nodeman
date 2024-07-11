43865b8e91f375eb296084f2a08944b60dc44c8a
yunchao <wyyalt@live.com>
2024-07-11 18:52:05 +0800

    feat: 适配JOB API限流错误码 (closed #2315)

--- apps/core/files/base.py
+++ apps/core/files/base.py
@@ -144,9 +144,9 @@ class BkJobMixin:
         """
         # 如果后续预处理的job api增多，考虑拆分逻辑
         if job_api_func.api_name == JobApi.fast_transfer_file.api_name:
-            query_params = deepcopy(query_params)
             query_params["file_source_list"] = self._handle_file_source_list(
-                file_source_list=query_params.get("file_source_list", []), extra_transfer_file_params=query_params
+                file_source_list=query_params.get("file_source_list", []),
+                extra_transfer_file_params=deepcopy(query_params),
             )
         return query_params
 
