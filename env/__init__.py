deecf1b11a0397fb9f3b86664dfc62badad0f030
dcd <1151627903@qq.com>
2024-12-26 16:32:41 +0800

    fix: 修复同步云服务商CC不存在某个云区域问题 (closed #2529)

--- env/__init__.py
+++ env/__init__.py
@@ -71,8 +71,6 @@ __all__ = [
     "BKAPP_DEFAULT_INSTALL_CHANNEL_ID",
     "BKAPP_AUTOMATIC_CHOICE_CLOUD_ID",
     "TXY_ENDPOINT",
-    # 未分配管控区域ID
-    "BKAPP_UNASSIGNED_CLOUD_ID",
 ]
 
 # ===============================================================================
@@ -99,8 +97,6 @@ BKAPP_IEOD_ACTIVE_FIREWALL_POLICY_SCRIPT_INFO = get_type_env(
 )
 BKAPP_DEFAULT_INSTALL_CHANNEL_ID = get_type_env(key="BKAPP_DEFAULT_INSTALL_CHANNEL_ID", default=-1, _type=int)
 BKAPP_AUTOMATIC_CHOICE_CLOUD_ID = get_type_env(key="BKAPP_AUTOMATIC_CHOICE_CLOUD_ID", default=-1, _type=int)
-# 适配从k8s读取值变成科学计数法问题
-BKAPP_UNASSIGNED_CLOUD_ID = int(get_type_env(key="BKAPP_UNASSIGNED_CLOUD_ID", default=90000001, _type=float))
 
 # ===============================================================================
 # 日志
