8b0b4635b1817cb1e84f3626265217aa33538a13
jpyoung3 <809608046@qq.com>
2025-04-24 16:03:07 +0800

    feat: 接入点租户隔离 # Reviewed, transaction id: 40256

--- common/api/utils/params.py
+++ common/api/utils/params.py
@@ -68,8 +68,7 @@ else:
         else:
             # _request，用于并发请求的场景
             _request = params.get("_request")
-            # req = _request or get_request()
-            req = get_request() or _request
+            req = _request or get_request()
             # 补充请求凭证，如bk_token
             auth_info = build_auth_args(req)
             params.update(auth_info)
