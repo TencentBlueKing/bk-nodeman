fa295c8dee517d4bcaa916d8581330b53475486d
jpyoung3 <809608046@qq.com>
2025-07-08 15:28:51 +0800

    feat: 未分配管控区域适配

--- apps/authentication.py
+++ apps/authentication.py
@@ -11,39 +11,62 @@ specific language governing permissions and limitations under the License.
 
 import logging
 
-from blueapps import metrics
+from apigw_manager.apigw.authentication import UserModelBackend
+
+# from blueapps import metrics
 from blueapps.account import get_user_model
-from django.contrib.auth.backends import ModelBackend
+
+# from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import AnonymousUser
 
 logger = logging.getLogger("component")
 
 
-class ApiGatewayJWTUserModelBackend(ModelBackend):
+class ApiGatewayJWTUserModelBackend(UserModelBackend):
     """Get users by username"""
 
-    def user_maker(self, bk_username):
+    def __init__(self):
+        super().__init__()
+
         user_model = get_user_model()
-        try:
-            user, _ = user_model.objects.get_or_create(defaults={"nickname": bk_username}, username=bk_username)
-        except Exception:
-            metrics.BLUEAPPS_USER_TOKEN_VERIFY_FAILED_TOTAL.labels(
-                hostname=metrics.HOSTNAME, token_type="bk_jwt", err="user_verify_err"
-            ).inc()
-            logger.exception(f"[{self.__class__.__name__}] Failed to get_or_create user -> {bk_username}.")
-            return None
-        else:
-            return user
+
+        # 未将用户保存到 db，防止未预期添加用户数据
+        # 未查询 db 中用户，因用户可能在 db 中不存在
+        self.user_maker = lambda username: user_model(**{user_model.USERNAME_FIELD: username})
+
+    # def user_maker(self, bk_username):
+    #     user_model = get_user_model()
+    #     try:
+    #         user, _ = user_model.objects.get_or_create(defaults={"nickname": bk_username}, username=bk_username)
+    #     except Exception:
+    #         metrics.BLUEAPPS_USER_TOKEN_VERIFY_FAILED_TOTAL.labels(
+    #             hostname=metrics.HOSTNAME, token_type="bk_jwt", err="user_verify_err"
+    #         ).inc()
+    #         logger.exception(f"[{self.__class__.__name__}] Failed to get_or_create user -> {bk_username}.")
+    #         return None
+    #     else:
+    #         return user
 
     def make_anonymous_user(self, bk_username=None):
         user = AnonymousUser()
         user.username = bk_username  # type: ignore
+        user.tenant_id = ""  # type: ignore
         return user
 
-    def authenticate(self, request, api_name, bk_username, verified, **credentials):
+    # def authenticate(self, request, api_name, bk_username, verified, **credentials):
+    #     if not verified:
+    #         metrics.BLUEAPPS_USER_TOKEN_VERIFY_FAILED_TOTAL.labels(
+    #             hostname=metrics.HOSTNAME, token_type="bk_jwt", err="user_verify_err"
+    #         ).inc()
+    #         return self.make_anonymous_user(bk_username=bk_username)
+    #     return self.user_maker(bk_username)
+
+    def authenticate(self, request, gateway_name, bk_username, verified, tenant_id="", **credentials):
+        logger.info(f"==========={gateway_name}, {bk_username}, {verified}, {tenant_id}, {credentials}=========")
         if not verified:
-            metrics.BLUEAPPS_USER_TOKEN_VERIFY_FAILED_TOTAL.labels(
-                hostname=metrics.HOSTNAME, token_type="bk_jwt", err="user_verify_err"
-            ).inc()
             return self.make_anonymous_user(bk_username=bk_username)
-        return self.user_maker(bk_username)
+        logger.info(f"{gateway_name}, {bk_username}, {verified}, {tenant_id}, {credentials}")
+        user = self.user_maker(bk_username)
+        user.tenant_id = tenant_id  # type: ignore
+        logger.info(f"========= username：{user.username} ========== tenant_id：{user.tenant_id} =========")
+        return user
