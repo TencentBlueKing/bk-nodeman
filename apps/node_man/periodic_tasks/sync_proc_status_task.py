4f8fd9daad71c04869f7cdb9f7092d0e12a28744
chalice-1831 <844589474@qq.com>
2025-08-20 19:35:12 +0800

    fix: 更新插件状态添加未注册插件信息 (closed #2415)

--- apps/node_man/periodic_tasks/sync_proc_status_task.py
+++ apps/node_man/periodic_tasks/sync_proc_status_task.py
@@ -156,6 +156,7 @@ def update_or_create_proc_status(
                     bk_host_id=agent_id__host_id_map[agent_id],
                     is_latest=True,
                 )
+                to_be_created_process_status_objs.append(obj)
 
         logger.info(
             f"{task_id} | sync_proc_status_task: Not need to update record "
