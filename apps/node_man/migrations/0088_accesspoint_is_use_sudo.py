c607b2f38ac6acef8f8afbaf7db60799cbc54cf0
chalice-1831 <844589474@qq.com>
2025-09-04 16:07:10 +0800

    feat: 更新冲突的migration文件名

--- apps/node_man/migrations/0088_accesspoint_is_use_sudo.py
+++ apps/node_man/migrations/0088_accesspoint_is_use_sudo.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("node_man", "0086_add_agent_config_plugin_ipc_and_alarm_event_data_id"),
+        ("node_man", "0087_update_ap_regionid_cityid"),
     ]
 
     operations = [
